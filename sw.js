4dc8100a6fcd5a57766534bcdd676584d7d73ef5
ASAN-INJU <jjukssam755@gmail.com>
2026-07-12 01:57:49 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='hi-v5';const A=['./','./index.html','./manifest.json','./icon.svg','./data/data.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
