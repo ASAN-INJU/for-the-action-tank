fa280ed912ab57f6cdc90a7cf81e806697625559
ASAN-INJU <jjukssam755@gmail.com>
2026-07-14 22:22:13 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE="hi-ultimate-v8-20260712";
-const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{
- if(e.request.method!=="GET")return;
- e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r})
- .catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
-});
